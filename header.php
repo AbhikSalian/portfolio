010f0279431732c7dfef9532d61d7e2f823916c9
Abhik Salian <21e03.abhik@sjec.ac.in>
2024-05-30 02:33:26 +0530

    demo

---
+++ header.php
@@ -0,0 +1,147 @@
+<html>
+    <head>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M5H47GQP');</script>
+<!-- End Google Tag Manager -->
+        <!-- favicon -->
+        <!-- responsive tag -->
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/png" href="https://ipossoft.com/wp-content/uploads/2022/10/ipos-favicon.png" />
+
+        <?php 
+            wp_head(); 
+        ?>
+        	
+<style>div.uMxONZEhUNlTQTkN3jDIjMhNjmzjOz1{display:none !important;}</style>
+<meta name="ahrefs-site-verification" content="68937e290fc1b8f351cc97d2940c4683aeb1c9b7679c3149a89f54d1ff1bac5a">
+<?php echo get_field('header_custom_code'); ?>    
+<!-- 		    <script src="https://www.google.com/recaptcha/api.js" async defer></script> -->
+
+</head>
+<body class="defult-home">
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M5H47GQP"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+     <!-- Main content Start -->
+<div class="main-content">
+ <!--Full width header Start-->
+ <div class="full-width-header">
+            <!--Header Start-->
+            <style>
+                /* This is for the new header design */
+                .full-width-header .rs-header .topbar-area .rs-contact-info .contact-part {
+                    padding: 0 25px;
+                    padding-top: 7px;
+                }
+            </style>
+
+
+           
+
+
+            <!--Header Start-->
+            <header id="rs-header" class="rs-header">
+                <!-- Topbar Area Start -->
+                <div class="topbar-area">
+                    <div class="container">
+                        <div class="row rs-vertical-middle">
+                            <div class="col-lg-3">
+                                <div class="logo-part">
+                                    <a href="<?php echo get_bloginfo('url') ?>"><img loading="lazy" src="https://ipossoft.com/wp-content/uploads/2023/12/LOGO-02.png" alt="logo"></a>
+                                </div>
+                            </div>
+                            <div class="col-lg-9 text-right">
+                                <ul class="rs-contact-info">
+                                   
+                                    <li class="contact-part">
+                                        <i class="flaticon-email"></i>
+                                        <span class="contact-info">
+                                            <span>E-mail</span>
+                                            <a href="mailto:info@ipossoft.com"> info@ipossoft.com</a>
+                                        </span>
+                                    </li>
+                                    <li class="contact-part">
+                                        <i class="flaticon-call"></i>
+                                        <span class="contact-info">
+                                            <span>Phone</span>
+                                            <a href="tel:+9714 4281510">+9714 4281510</a>
+                                        </span>
+                                    </li>
+                                    <li class="contact-part no-border">
+                                        <i class="flaticon-phone"></i>
+                                        <span class="contact-info">
+                                            <span>Mobile</span>
+                                            <a href="tel:+971 525226868">+971 525226868</a>
+                                        </span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Topbar Area End -->
+
+                <!-- Menu Start -->
+                <div class="menu-area menu-sticky">
+                    <div class="container">
+                        <div class="logo-area">
+                            <a href="<?php echo get_bloginfo('url') ?>">
+                                <img loading="lazy" class="sticky-logo" src="https://ipossoft.com/wp-content/uploads/2023/12/LOGO-02.png" alt="logo">
+                            </a>
+                        </div>
+                        <div class="rs-menu-area">
+                            <div class="main-menu">
+                                <div class="mobile-menu">
+                                    <a href="<?php echo get_bloginfo('url') ?>" class="mobile-logo">
+                                        <img loading="lazy" src="https://ipossoft.com/wp-content/uploads/2023/12/LOGO-02.png" alt="logo">
+                                    </a>
+                                    <a href="#" class="rs-menu-toggle rs-menu-toggle-close">
+                                        <i class="fa fa-bars"></i>
+                                    </a>
+                                </div>
+                                <nav class="rs-menu rs-menu-close">
+                                    <?php
+                                        wp_nav_menu( 
+                                                array( 
+                                                    'theme_location' => 'header-menu',
+                                                    'menu_class' => 'nav-menu',
+                                                    'menu_id'   => 'onepage-menu', 
+                                                ) 
+                                            );
+                                    ?>
+                                   
+                                </nav>
+                            </div> <!-- //.main-menu -->
+                        </div>
+                        <div class="expand-btn-inner search-icon hidden-sticky hidden-md">
+                            <ul>
+                               
+                                <li><a class="quote-btn-light" href="<?php echo get_bloginfo('url').'/request-demo/'; ?>">Request Demo</a></li>
+                            </ul>
+                            <div class="toolbar-sl-share">
+                                <ul class="social">
+                                    <li><a href="https://www.facebook.com/iPOSRetail/" target="_blank"><i
+                                                class="fa fa-facebook" rel="noopener noreferrer"></i></a></li>
+                                    <li><a href="https://www.instagram.com/ipossoft?igsh=MWE0dndwOG0xZmVmMg==" target="_blank"><i
+                                                class="fa  fa-instagram" rel="noopener noreferrer"></i></a></li>
+                                    <li><a href="https://in.linkedin.com/company/i-pos-softnet-fzc" target="_blank"><i
+                                                class="fa fa-linkedin" target="_blank" rel="noopener noreferrer"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Menu End -->
+            </header>
+            <!--Header End-->
+
+            <!--Header End-->
+        </div>
+        <!--Full width header End-->
+
